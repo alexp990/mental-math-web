5725e2e267db61ee3e5a1363a734c970d66a8d04
andgrewdd <andre.pvlv20@gmail.com>
2026-07-14 11:44:23 +0100

    2.d.p ratio on endscreen

--- src/ScoreScreen.jsx
+++ src/ScoreScreen.jsx
@@ -22,7 +22,7 @@ export function ScoreScreen({ score, onGoHome, wrong, time }) {
       <p className='text-4xl text-amber-400'>Your score: {score}</p>
       <p className='text-4xl text-red-600'>Number of wrong answers: {wrong}</p>
       <p className='text-4xl text-green-500'>Percentage: {`${(score/(score+wrong)).toFixed(2)*100}%`}</p>
-      <p className='text-4xl text-green-500'>Score/Time ratio:{score/time}</p>
+      <p className='text-4xl text-green-500'>Score/Time ratio:{(score/time).toFixed(2)}</p>
       <Button className='bg-blue-300 hover:bg-blue-200 w-fit p-6' onClick={onGoHome}>Home</Button>
     </div>
   );
